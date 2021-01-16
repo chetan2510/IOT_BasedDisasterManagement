21d9066c9d42df555e089d02a4047548a20e265e
mubashir2994 <ali.mubashir2993@yahoo.com>
2021-01-16 22:23:50 +0100

    upload

---
+++ locationA/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import {MatSelectModule} from '@angular/material/select';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LandingComponentsComponent } from './landing-components/landing-components.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LandingComponentsComponent
+  ],
+  imports: [
+    BrowserModule,
+    MatSelectModule,
+    BrowserAnimationsModule,
+    NgbModule,
+   // NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

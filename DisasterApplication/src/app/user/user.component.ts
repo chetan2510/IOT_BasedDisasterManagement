344f982ca3bc04fbf13f0d8bdaf3ea5abb9fa84a
mubashir2994 <ali.mubashir2993@yahoo.com>
2021-02-27 16:01:07 +0500

    Added FavIcon, Background Image and Uni Logo

--- DisasterApplication/src/app/user/user.component.ts
+++ DisasterApplication/src/app/user/user.component.ts
@@ -138,7 +138,7 @@ public changeSelectionOverviewExample(value){
        this.mymap = L.map('map').setView(latLong, 10);
 
       L.tileLayer(
-        'assets/Egypt/{z}/{x}/{y}.png',
+        'assets/Pakistan/{z}/{x}/{y}.png',
         {
           // attribution:
           //   'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
